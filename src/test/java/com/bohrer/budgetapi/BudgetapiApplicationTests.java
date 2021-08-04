6d64804976fdc7d03395549e657ae5125a057773
RickC137 <bthebuilder447@gmail.com>
2021-08-04 17:00:55 -0400

    initial build

---
+++ src/test/java/com/bohrer/budgetapi/BudgetapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohrer.budgetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
